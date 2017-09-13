582f252e616784a1f3f4e59d2b2f0086588e808c
e-sqwared <eisenstein.eli@gmail.com>
2017-09-13 12:38:30 -0400

    aww heck

---
+++ euler1.py
@@ -0,0 +1,15 @@
+print(sum(range(0,1000,3)) + sum(range(0,1000,5)) - sum(range(0,1000,15)))
+
+##total_sum = 0
+##
+##print ("Welcome NICK RENNER \n\n")
+##
+##rangenum = int(input("Enter a number: "))
+##modnum1 = int(input("Enter the first factor: "))
+##modnum2 = int(input("Enter the second factor: "))
+##
+##for i in range(1, rangenum):
+##    if (i % modnum1 == 0 or i % modnum2 == 0):
+##        total_sum = total_sum + i
+##print (total_sum)
+

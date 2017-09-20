e0a7246217359170a12d3d7b961f25751bcdd9f0
Eli Eisenstein <eeisenstein@Elis-MacBook-Pro-2.local>
2017-09-20 12:08:30 -0400

    lit game

---
+++ AdventureGame/actions.py
@@ -0,0 +1,13 @@
+# -*- coding utf-8 -*-
+
+import Player
+
+class Action():
+    def __init__(self, method, name, hotkey, **kwargs):
+        self.method = method
+        self.hotkey = hotkey
+        self.name = name
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return "{}: {}".format(self.hotkey, self.name)
---
+++ object_oriented_1.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+class BankAccount(object):
+    """defines the BankAccount"""
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        self.balance -= amount
+    def overdrawn(self):
+        return self.balance < 0
+
+my_account = BankAccount(15)
+##teller = raw_input("Would you like to withdraw or deposit?")
+##etc
+my_account.withdraw(5)
+print my_account.balance
